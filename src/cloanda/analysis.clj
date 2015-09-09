75f5345daeec7f18ef1c81a8cc02de101bc6b510
yellowbean <always.zhang@gmail.com>
2015-09-09 23:01:01 +0800

    add cross over

--- src/cloanda/analysis.clj
+++ src/cloanda/analysis.clj
@@ -1,4 +1,11 @@
-(ns cloanda.analysis)
+(ns cloanda.analysis
+  (:import
+   (org.apache.commons.math3.stat.descriptive DescriptiveStatistics)
+   (org.apache.commons.math3.stat.regression SimpleRegression))
+  )
+
+
+;;;;;; technical indicators
 
 (defn avg [v] (/ (reduce + v) (count v)))
 
@@ -8,7 +15,18 @@
   )
 
 (defn ema [v w alpha]
-  (loop [ s  (subvec v w)  r [ (nth v w)  ] ]
+  (loop [ s  (subvec v w)  r [ (nth v w)] ]
     (if (= (dec w) (count s))
       r
       (recur (next s) (conj r (+ (* alpha (first s) ) (* (- 1 alpha) (last r))))))))
+
+;;;;;; signals
+(defn cross-over [fast slow]
+  (let [ pairs (map vector fast slow)]
+    (map #(- %1 %2) pairs)
+
+    )
+  )
+
+
+;;;;;; strategy
