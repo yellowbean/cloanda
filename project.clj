78389002e70fcf97b4d5003bb71e268314449628
zhang <zhang@citizenz>
2016-05-31 00:01:32 +0800

    Refactor MACRO

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject cloanda "0.1.0-SNAPSHOT"
+(defproject cloanda "0.1.1-SNAPSHOT"
   :description "A clojure wrapper for Oanda REST API"
   :url "https://github.com/yellowbean/cloanda"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "1.0.1"]
                  [clj-json "0.5.3"]
                  [org.clojure/tools.logging "0.3.1"]
-		[org.apache.commons/commons-math3 "3.5"]
 		]
   :main ^:skip-aot cloanda.core
   :target-path "target/%s"
